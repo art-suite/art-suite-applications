7db64ec97b3e90d39f222216d0f6f6fc24691d86
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2019-07-30 10:57:23 -0700

    PACKAGE - dependencies

--- packages/art-markdown/source/Art.Markdown/_Client/index.js
+++ packages/art-markdown/source/Art.Markdown/_Client/index.js
@@ -4,6 +4,7 @@
 (module.exports = require('./namespace'))
 
 .addModules({
+  CodePalette:    require('./CodePalette'),
   MarkdownView:   require('./MarkdownView'),
   MenuMarkdown:   require('./MenuMarkdown'),
   StandardImport: require('./StandardImport')
