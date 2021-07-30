0fe0d5ce269ff7a1cf946a82f16ecc9565cfebe5
shanebdavis <shanebdavis@gmail.com>
2021-07-30 09:04:44 -0700

    converting tests to caf

--- packages/art-ery/test/tests/Art.Ery/ClientServer/Pipelines/index.js
+++ packages/art-ery/test/tests/Art.Ery/ClientServer/Pipelines/index.js
@@ -4,7 +4,6 @@
 (module.exports = require('./namespace'))
 
 .addModules({
-  Auth:           require('./Auth'),
   ClientFailures: require('./ClientFailures'),
   CompoundKeys:   require('./CompoundKeys'),
   FilterLocation: require('./FilterLocation'),
