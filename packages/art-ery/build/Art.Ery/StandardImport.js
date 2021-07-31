5f4bce488cacd604fe4cea340aa1f1d436d315e2
shanebdavis <shanebdavis@gmail.com>
2021-07-31 15:31:36 -0700

    patch/refactor: Everything that can be converted to Caf has been converted. Once ArtModels is also converted, we can discard the remaining coffeescript

--- packages/art-ery/build/Art.Ery/StandardImport.js
+++ packages/art-ery/build/Art.Ery/StandardImport.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return require('art-standard-lib').mergeWithSelf(require('art-class-system'), require('art-communication-status'), require('art-validation'), require('./Stack'), require('./Env'), require('./Config'));});
+Caf.defMod(module, () => {return [require('art-standard-lib'), require('art-class-system'), require('art-communication-status'), require('art-validation'), require('./Stack'), require('./Env'), require('./Config')];});
 //# sourceMappingURL=StandardImport.js.map
