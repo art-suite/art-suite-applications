a4c2795edab996298dd8c4969e43569113221043
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2022-03-08 16:23:46 -0800

    wip: converted to modern build system for ArtEry

---
+++ packages/art-ery/build/Art.Ery/Filters/LinkFieldsFilter2Transition2BFilter.js
@@ -0,0 +1,4 @@
+"use strict"
+let Caf = require('caffeine-script-runtime');
+Caf.defMod(module, () => {return (() => {let LinkFieldsFilter2Transition2BFilter; return LinkFieldsFilter2Transition2BFilter = Caf.defClass(class LinkFieldsFilter2Transition2BFilter extends require('../Filter') {}, function(LinkFieldsFilter2Transition2BFilter, classSuper, instanceSuper) {this.location("client"); this.before({all: function(response) {return response.withMergedProps({acceptLinkFieldsFilter2Encoding: true});}});});})();});
+//# sourceMappingURL=LinkFieldsFilter2Transition2BFilter.js.map
---
+++ packages/art-ery/build/index.js
@@ -0,0 +1,4 @@
+"use strict"
+let Caf = require('caffeine-script-runtime');
+Caf.defMod(module, () => {return require('./Art.Ery');});
+//# sourceMappingURL=index.js.map
