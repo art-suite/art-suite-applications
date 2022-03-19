46b69bffc3200f8057683be20f30233fdb1b5f64
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2022-03-19 15:26:32 -0700

    patch/improvement: verify art-pusher working E2E

--- packages/art-suite-server/build/Art.Suite.Server/StandardImport.js
+++ packages/art-suite-server/build/Art.Suite.Server/StandardImport.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return [require('art-standard-lib'), require('art-html-factory'), require('art-express-server'), require('art-config'), require('./Lib'), require('./Config')];});
+Caf.defMod(module, () => {return [require('art-standard-lib'), require('art-html-factory'), require('art-config'), require('art-express-server'), require('./Lib'), require('./Config')];});
 //# sourceMappingURL=StandardImport.js.map
