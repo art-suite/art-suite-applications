3b97e906b5a3d81bac8b94a2bb780d600120c3f9
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2022-03-26 16:49:18 -0700

    wip: art-ery-core experiment - fun - using TDD!

---
+++ unpublished/art-ery-core/build/test/StandardImport.js
@@ -0,0 +1,4 @@
+"use strict"
+let Caf = require('caffeine-script-runtime');
+Caf.defMod(module, () => {return [require('art-standard-lib'), require('art-testbench'), require('../Art.Ery.Core')];});
+//# sourceMappingURL=StandardImport.js.map
