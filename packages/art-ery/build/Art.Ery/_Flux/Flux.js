aaec10e29bf593a3cf2c20ea8cd628347a44b125
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2023-09-21 14:16:44 +0200

    patch/refactor: PrefetchedRecordCache now attached to PipelineRegistry instances

--- packages/art-ery/build/Art.Ery/_Flux/Flux.js
+++ packages/art-ery/build/Art.Ery/_Flux/Flux.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {let Error = global.Error; return {defineModelsForAllPipelines: function() {return (() => {throw new Error("DEPRICATED: &ArtEry/Flux.defineModelsForAllPipelines()\nUSE: &@ArtSuite/ArtPipelineModles.defineModelsForAllPipelines()");})();}, defineArtEryPipelineFluxModels: function() {return (() => {throw new Error("DEPRICATED: &ArtEry/Flux.defineArtEryPipelineFluxModels()\nUSE: &@ArtSuite/ArtPipelineModles.defineModelsForAllPipelines()");})();}};});
+Caf.defMod(module, () => {let Error = global.Error; return {defineModelsForAllPipelines: function() {return (() => {throw new Error("DEPRICATED: &ArtEry/Flux.defineModelsForAllPipelines()\nUSE: &@ArtSuite/ArtPipelineModels.defineModelsForAllPipelines()");})();}, defineArtEryPipelineFluxModels: function() {return (() => {throw new Error("DEPRICATED: &ArtEry/Flux.defineArtEryPipelineFluxModels()\nUSE: &@ArtSuite/ArtPipelineModels.defineModelsForAllPipelines()");})();}};});
 //# sourceMappingURL=Flux.js.map
