1b09b2758a585e5a2439c6e8c1d2d551b808c28b
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2023-09-23 18:33:12 +0200

    patch/fix: art-ery-aws passing

--- packages/@art-suite/art-pipelines/build/ArtSuite/ArtPipelines/Config.js
+++ packages/@art-suite/art-pipelines/build/ArtSuite/ArtPipelines/Config.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return Caf.importInvoke(["Configurable"], [global, require('./StandardImport'), require('art-config')], (Configurable) => {let Config; return Config = Caf.defClass(class Config extends Configurable {}, function(Config, classSuper, instanceSuper) {this.defaults({verbose: false, returnProcessingInfoToClient: false});});});});
+Caf.defMod(module, () => {return Caf.importInvoke(["Configurable"], [global, require('./StandardImport'), require('art-config')], (Configurable) => {let Config; return Config = Caf.defClass(class Config extends Configurable {}, function(Config, classSuper, instanceSuper) {this.defaults({verbose: false, returnProcessingInfoToClient: false, saveSessions: true});});});});
 //# sourceMappingURL=Config.js.map
--- packages/art-ery/Flux.js
+++ packages/art-ery/Flux.js
@@ -1,1 +1,12 @@
-module.exports = require("./build/Art.Ery/_Flux");
+module.exports = {
+  defineModelsForAllPipelines: function () {
+    throw new Error(
+      "DEPRICATED: &ArtEry/Flux.defineModelsForAllPipelines()\nUSE: &@ArtSuite/ArtPipelineModels.defineModelsForAllPipelines()"
+    );
+  },
+  defineArtEryPipelineFluxModels: function () {
+    throw new Error(
+      "DEPRICATED: &ArtEry/Flux.defineArtEryPipelineFluxModels()\nUSE: &@ArtSuite/ArtPipelineModels.defineModelsForAllPipelines()"
+    );
+  },
+};
