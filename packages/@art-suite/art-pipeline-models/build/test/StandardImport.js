6055de69aef01597e2a619be00897ef6fd8bae7a
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2023-10-20 12:10:39 +0200

    patch/fix: removed remaining art-ery refs; but broke one test

--- packages/@art-suite/art-pipeline-models/build/test/StandardImport.js
+++ packages/@art-suite/art-pipeline-models/build/test/StandardImport.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {require('art-config').configure(); return [require('art-standard-lib'), require('art-class-system'), require('art-testbench'), require('art-communication-status'), require('art-ery'), require('@art-suite/art-models'), require('../../build')];});
+Caf.defMod(module, () => {require('art-config').configure(); return [require('art-standard-lib'), require('art-class-system'), require('art-testbench'), require('art-communication-status'), require('@art-suite/art-pipelines'), require('@art-suite/art-models'), require('../../build')];});
 //# sourceMappingURL=StandardImport.js.map
