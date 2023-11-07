6a49334e2093e7eb9af57f3aa3d1dd2c50810a57
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2023-11-07 16:22:58 +0100

    patch/test: added Auth scenario

--- packages/@art-suite/art-pipelines/build/test/Scenarios/index.js
+++ packages/@art-suite/art-pipelines/build/test/Scenarios/index.js
@@ -4,6 +4,8 @@
 (module.exports = require('./namespace'))
 
 .addModules({
+  AuthPipeline:       require('./AuthPipeline'),
+  AuthPipelineTest:   require('./AuthPipeline.test'),
   SimplePipeline:     require('./SimplePipeline'),
   SimplePipelineTest: require('./SimplePipeline.test')
 });
