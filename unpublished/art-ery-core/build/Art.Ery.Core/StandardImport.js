789c3d42b44df6817bafab256de4d7b248391c56
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2023-11-12 14:22:01 +0100

    patch/refactor: don't need art-ery-core now; art-pipelines is great

--- unpublished/art-ery-core/build/Art.Ery.Core/StandardImport.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return [require('art-standard-lib'), require('art-class-system'), require('art-communication-status'), require('./Lib').Stack];});
-//# sourceMappingURL=StandardImport.js.map
