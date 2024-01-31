578007b7a51d034646d65a1675f466b88b5c2d66
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2024-01-31 10:47:49 +0100

    patch/tooling: updated build

--- packages/art-atomic/source/jest/Color.more.test.js
+++ packages/art-atomic/source/jest/Color.more.test.js
@@ -1,23 +1,4 @@
-"use strict";
-let Caf = require("caffeine-script-runtime");
-Caf.defMod(module, () => {
-  return Caf.importInvoke(
-    ["describe", "test", "assert", "rgbColor"],
-    [global, require("./StandardImport")],
-    (describe, test, assert, rgbColor) => {
-      return describe({
-        with: function () {
-          test("withHue", () =>
-            assert.eq(rgbColor("#f00"), rgbColor("#0f0").withHue(0)));
-          return test("withSimilarHue", () => {
-            assert.eq(rgbColor("#f00"), rgbColor("#0f1").withHue(0));
-            return assert.eq(
-              rgbColor("#f10").hexString,
-              rgbColor("#0f1").withSimilarHue(0).hexString
-            );
-          });
-        },
-      });
-    }
-  );
-});
+"use strict"
+let Caf = require('caffeine-script-runtime');
+Caf.defMod(module, () => {return Caf.importInvoke(["describe", "test", "assert", "rgbColor"], [global, require('./StandardImport')], (describe, test, assert, rgbColor) => {return describe({with: function() {test("withHue", () => assert.eq(rgbColor("#f00"), rgbColor("#0f0").withHue(0))); return test("withSimilarHue", () => {assert.eq(rgbColor("#f00"), rgbColor("#0f1").withHue(0)); return assert.eq(rgbColor("#f10").hexString, rgbColor("#0f1").withSimilarHue(0).hexString);});}});});});
+//# sourceMappingURL=Color.more.test.js.map
