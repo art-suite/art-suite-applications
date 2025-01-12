31b6ec7256288affc9765c805bb346c978c9ba55
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2025-01-12 14:24:34 +0100

    patch/fix: fixed Neptune Namespace issue

--- packages/art-suite-server/build/index.js
+++ packages/art-suite-server/build/index.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return require('./Art.Suite.Server');});
+Caf.defMod(module, () => {return require('./Art.SuiteServer');});
 //# sourceMappingURL=index.js.map
